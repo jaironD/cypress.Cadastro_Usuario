e3807c981ccfb93a60384cf70c043995a1ad8c8e
Jairon Douglas <53066973+jaironD@users.noreply.github.com>
2022-07-21 19:46:54 -0300

    Create cadastro.cy.js

---
+++ cadastro.cy.js
@@ -0,0 +1,39 @@
+/// <reference types="cypress" /> 
+
+
+
+context('Teste_site_compras',() =>{
+    it("Realizar cadastro", () =>{
+        cy.visit('https://cliente.americanas.com.br/minha-conta/cadastro?next=https://www.americanas.com.br/')
+
+        cy.get('[data-cy=formField__name]')
+            .type('João da Silva')
+
+        cy.get('[data-cy=labelFor__M]')
+            .click()
+
+        cy.get('[data-cy=formField__birthDate]')
+        .type('01/04/2022')
+
+        cy.get('[data-cy=formField__cpf]')
+            .type('142.032.750-08')
+
+        cy.get('[data-cy=formField__phone]')
+            .type('(61) 92833-4384')
+
+        cy.get('[data-cy=formField__email]')
+            .type('danilo-porto94@attglobal.net')
+
+        cy.get('[data-cy=formField__promotionalEmails]')
+            .click()
+
+        cy.get('[data-cy=formField__password]')
+            .type('J@123456')
+
+        cy.get('[data-cy=formField__dataSharingThirdParty]')
+            .click()
+
+        cy.get('[data-cy=sign-submit]')
+            .click()
+    })
+})
